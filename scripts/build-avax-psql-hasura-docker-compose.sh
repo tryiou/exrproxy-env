624558f9d6a083b6f4dfa2ea1a840324e0ae0c4a
pom11 <parsecoriginalmastercraft@gmail.com>
2021-04-20 01:15:19 +0300

    Update build-avax-psql-hasura-docker-compose.sh

--- scripts/build-avax-psql-hasura-docker-compose.sh
+++ scripts/build-avax-psql-hasura-docker-compose.sh
@@ -7,17 +7,22 @@
 # 	rm	
 # 	sed
 
-#clone avax
-echo "cloning avax"
-git clone https://github.com/ava-labs/avalanchego.git >/dev/null 2>&1
+read -p 'Build avalanchego? [y/n] ' avax
+if [ $avax = "y" ]; then
+   #clone avax
+	echo "cloning avax"
+	git clone https://github.com/ava-labs/avalanchego.git >/dev/null 2>&1
 
-#build docker image
-echo "building docker image"
-sh avalanchego/scripts/build_image.sh >/dev/null 2>&1
+	#build docker image
+	echo "building docker image"
+	sh avalanchego/scripts/build_image.sh >/dev/null 2>&1
 
-#delete cloned git folder
-echo "remove avax folder"
-rm -rf avalanchego >/dev/null 2>&1
+	#delete cloned git folder
+	echo "remove avax folder"
+	rm -rf avalanchego >/dev/null 2>&1
+else
+  echo "Not cloning avalanchego"
+fi
 
 #echo image with tag
 echo "docker image:"
@@ -25,5 +30,13 @@ VAR=$(docker images | grep avaplatform/avalanchego | awk '{print $1":"$2}')
 echo $VAR
 
 #replace in template docker image
-echo "generating docker-compose"
+echo "generating docker-compose.yaml"
 sed "s~AVAX_IMAGE~$VAR~g" template-avax-psql-hasura.yaml > docker-compose.yaml
+
+
+read -p 'Run docker-compose up? [y/n] ' compose
+if [ $compose = "y" ]; then
+   docker-compose up
+else
+  echo "Not running docker-compose"
+fi
